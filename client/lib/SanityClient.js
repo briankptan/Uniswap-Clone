fef7066758255c66d3948b3d4035b21ec315bcc7
Brian Tan <50221165+briankptan@users.noreply.github.com>
2022-04-28 21:13:31 -0700

    Add files via upload

---
+++ client/lib/SanityClient.js
@@ -0,0 +1,10 @@
+import sanityClient from '@sanity/client'
+
+export const client = sanityClient({
+    projectId: '3akp2ruh',
+    dataset: 'production',
+    apiVersion: 'v1',
+    token: 
+    'sk43RPAFiSl9qmUGrDWKN62VqD3sM3i4UUjMGTF91oQAJGslXQiLooCKK0GSfeVgjO9KHCTj9zyZIatJM5SlLezrhnzvVkDOqJycJEsymSoSRqLMvW7pLUJtZrd4lRZY8DXrgYFtiOxAMT1Sg31mZGOv0jnv3iuUr1X4ZQWCnG3SOjMhFY5W',
+    useCdn: false
+})
---
+++ client/lib/constants.js
@@ -0,0 +1,5 @@
+import abi from './Transactions.json'
+
+export const contractABI = abi.abi
+
+export const contractAddress = '0x4E00ec31c36F55384df18aE26ed19195271De439'
